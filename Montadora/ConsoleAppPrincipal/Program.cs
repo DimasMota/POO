75ba23b5019154c434d13ba6a8bcc6c2defd1495
Dimas <123428856+DimasMota@users.noreply.github.com>
2023-02-02 20:13:35 -0300

    02_de_fevereiro_de_2023 1

--- Montadora/ConsoleAppPrincipal/Program.cs
+++ Montadora/ConsoleAppPrincipal/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            
+            Caneta caneta = new Caneta();
+
         }
     }
 }
--- Montadora/ModelsDes/Caneta.cs
+++ Montadora/ModelsDes/Caneta.cs
@@ -1,7 +1,30 @@
-﻿namespace ModelsDes
+﻿using System.Runtime;
+
+namespace ModelsDes
 {
-    public class Class1
+    public class Caneta
     {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Cor { get; set; }
+        public string CorTinta { get; set; }
+        public bool Nova { get; set; }
+        public int PercentualTinta { get; set; }
+
+        
+        
+        
+        public void Escrever(int _escrita)
+        {
+            PercentualTinta = PercentualTinta - _escrita;
+            Nova = false;
+            if(PercentualTinta < 0)
+            {
+                PercentualTinta = 0;
+            }
+        }
 
     }
+
+   
 }
