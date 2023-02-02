a42077c26fd377cdcf0860f85867394fa0f1ff56
Dimas <123428856+DimasMota@users.noreply.github.com>
2023-02-01 21:34:43 -0300

    01_de_fevereiro_2023
    eita eita

--- Montadora/ConsoleAppPrincipal/Program.cs
+++ Montadora/ConsoleAppPrincipal/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace ConsoleAppPrincipal
+﻿using Models;
+
+namespace ConsoleAppPrincipal
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Pneu pneu1 = new Pneu(16, 150, false, "Carro de passeio");
+           
+           
+
+
             
+
+            Pneu pneu2 = new Pneu(16, 70, true, "Pneu de Estepe");
+           
+           
+            
+
+            pneu1.Girar(6);
+            pneu2.Girar(6);
+            pneu2.Girar(15);
+            pneu2.Girar(130);
+            pneu2.Frear(10);
+            pneu1.Exibir();
+            Console.WriteLine("\n\n");
+            pneu2.Exibir();
         }
+        
+
+
     }
 }
--- Montadora/Models/Pneu.cs
+++ Montadora/Models/Pneu.cs
@@ -2,6 +2,7 @@
 {
     public class Pneu
     {
+        // atributos ou propriedades
         public string Cor { get; set; }
         public int Aro { get; set; }
         public string Tipo { get; set; }
@@ -12,6 +13,22 @@
         public int VelocidadeAtual { get; set; }
         public bool Estepe { get; set; }
 
+        
+        public Pneu(int _aro, int _velocidade, bool _estepe, string _tipo) // constror feito com ctor 
+        {
+            Aro = _aro;
+            VelocidadeMaxima = _velocidade;
+            Estepe = _estepe;
+            Tipo = _tipo;
+
+            VelocidadeAtual = 0;
+            Estourado = false;
+            TWI = false;
+            Cor = "Preta";
+            PercentualBorracha = 100;
+        }
+
+        // metodos (aççao
        public void Girar(int _velocidade)
         {
             VelocidadeAtual = VelocidadeAtual + _velocidade;
@@ -31,7 +48,7 @@
         public void Frear(int _reducao)
         {
             VelocidadeAtual = VelocidadeAtual - _reducao;
-            PercentualBorracha = PercentualBorracha - 3;
+            PercentualBorracha = PercentualBorracha - 5;
 
             if (PercentualBorracha <= 30)
             {
@@ -42,6 +59,19 @@
                 VelocidadeAtual = 0;
             }
         }
+        public void Exibir()
+        {
+            Console.WriteLine("Aro: " + Aro);
+            Console.WriteLine("Percentual de Borracha: " + PercentualBorracha);
+            Console.WriteLine("Cor: " + Cor);
+            Console.WriteLine("Velocidade Máxima: " + VelocidadeMaxima);
+            Console.WriteLine("Estepe: " + Estepe);
+            Console.WriteLine("Estourado: " + Estourado);
+            Console.WriteLine("Tipo: " + Tipo);
+            Console.WriteLine("TWI: " + TWI);
+            Console.WriteLine("Velocidade Atual: " + VelocidadeAtual);
+        }
+
 
     }
 }
