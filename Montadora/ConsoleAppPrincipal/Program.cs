0758f94a675fb0fc9ee662c9b6b538be93525fd9
Dimas <123428856+DimasMota@users.noreply.github.com>
2023-02-02 20:45:24 -0300

    02_de_fevereiro_de_2023
    Inserção das características da caneta

--- Montadora/ConsoleAppPrincipal/Program.cs
+++ Montadora/ConsoleAppPrincipal/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace ConsoleAppPrincipal
+﻿using ModelsDes;
+
+namespace ConsoleAppPrincipal
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Caneta caneta = new Caneta();
+            Caneta caneta1 = new Caneta();
+            caneta1.Marca = "Piloto";
+            caneta1.Modelo = "Style";
+            caneta1.Cor = "Preta";
+            caneta1.CorTinta = "Azul";
+            caneta1.Nova = true;
+            caneta1.PercentualTinta = 100;
+
+
+            Console.WriteLine(caneta1.Marca);
+        
 
         }
     }
